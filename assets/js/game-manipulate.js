50a8bb3299261ed59359fc4e09e2e13229504368
Mehdi-Hp <mehdi.hoseinipajooh@gmail.com>
2015-06-06 16:31:18 +0430

    Visual changes

--- assets/js/game-manipulate.js
+++ assets/js/game-manipulate.js
@@ -203,8 +203,7 @@ var showGames = function() {
          gameDOM = '\
             <section class="game" id="game">\
                <section class="game__details">\
-                     <p class="game__day">' + gamesParsed.days[i] + '</p>\
-                     <p class="game__date">' + gamesParsed.dates[i] + '</p>\
+                     <p class="game__day">' + gamesParsed.days[i] + '<br>' + gamesParsed.dates[i] + '</p>\
                      <p class="game__time">' + gamesParsed.times[i] + '</p>\
                      <p class="game__series">' + gamesParsed.series[i] + '</p>\
                </section>\
