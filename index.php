65af26b8bff3af43d1cabdb11927bb541d871c91
Mehdi-Hp <mehdi.hoseinipajooh@gmail.com>
2015-06-14 12:32:06 +0430

    Animation added

--- index.php
+++ index.php
@@ -15,12 +15,33 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="assets/css/main.css" rel="stylesheet">
 		<link href="assets/css/css-reset.css" rel="stylesheet">
+
+		<link rel="shortcut icon" href="assets/images/favicons/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/images/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
+		<meta name="msapplication-square70x70logo" content="assets/images/favicons/smalltile.png" />
+		<meta name="msapplication-square150x150logo" content="assets/images/favicons/mediumtile.png" />
+		<meta name="msapplication-wide310x150logo" content="assets/images/favicons/widetile.png" />
+		<meta name="msapplication-square310x310logo" content="assets/images/favicons/largetile.png" />
+
+
 		<script src="assets/js/jquery.min.js"></script>
 		<script src="assets/js/persianumber.min.js"></script>
 		<script src="assets/js/game-manipulate.js"></script>
 	</head>
 	<body>
-		<span id="data-holder">
+		<span id="data-holder" style="visibility: hidden;">
 			<?php
 			if(include 'simple_html_dom.php') {
 				$html = file_get_html( 'http://varzesh3.com/');
